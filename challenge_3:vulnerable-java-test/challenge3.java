42856bf80687168f1157b33e14f5a5e764907493
Khang96 <nnbaokhang@gmail.com>
2021-05-17 00:27:48 -0600

    Update challenge3.java

--- challenge_3:vulnerable-java-test/challenge3.java
+++ challenge_3:vulnerable-java-test/challenge3.java
@@ -1,5 +1,7 @@
 //Example case 1
-String sql_query = "SELECT * FROM users WHERE userid ='"+ username + "'" + " AND password='" + Base64.getEncoder().encodeToString(passwordString.getBytes()) + "'";
+//"SELECT * FROM users WHERE userid ='"+ username + "'" + " AND password='" + Base64.getEncoder().encodeToString(passwordString.getBytes()) + "'";
+//Base64.getEncoder().encodeToString(passwordString.getBytes())
+String sql_query = "SELECT * FROM users WHERE userid =? AND password=?";   // zero trust
 Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(query);
 //End of example
@@ -7,13 +9,13 @@ ResultSet rs = stmt.executeQuery(query);
 //Example case 2
 public void postToMessageBoard(MessageBoard mb){
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	String s = br.readLine(); //VIOLATION
+	String s = br.readLine(); //VIOLATION, Need to sanitize userinput  zero trust
 	mb.post(s)
 }
 //End of Example
 
 //Example case 3
 String url = request.getParameter("hidden_url"); // not so hidden...
-URL url = new URL(url); // VIOLATION 
+URL url = new URL(url); // VIOLATION , url is user_input, perform zero trust
 InputStream is = url.openConnection().getContent();
 //End of Example
